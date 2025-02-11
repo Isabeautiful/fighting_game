6a8e03fa1f99085f1d9e523763122bb69ac7e074
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-02-11 14:17:08 -0300

    create a static player and enemy

--- index.js
+++ index.js
@@ -4,4 +4,34 @@ const c = canvas.getContext('2d'); //canvas context
 canvas.width = 1024;
 canvas.height = 576;
 
+//Using the canvas api, fillRect fills the screen with a black rectangle, starting at the top of the canvas (0,0) and going to the end of width and height
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+///creating a player
+class Sprite {
+  constructor(position) {
+    this.position = position;
+  }
+
+  draw(){
+    c.fillStyle = 'red';
+    c.fillRect(this.position.x, this.position.y, 50,150)
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 0
+})
+
+player.draw();
+
+const enemy = new Sprite({
+  x: 400,
+  y: 100
+})
+
+enemy.draw();
+
+console.log(player);
