762591731fe502342e9d9f731f743da2eb105719
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-02-12 14:13:33 -0300

    create attack for the enemy

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const STRENGHT_J = 20;
 
 ///creating a sprite class for players and enemies
 class Sprite {
-  constructor({position, velocity, color = 'red'}) {
+  constructor({position, velocity, color = 'red', offset}) {
     this.position = position;
     this.velocity = velocity;
     this.width = 50;
@@ -22,7 +22,11 @@ class Sprite {
     this.isJumping = false;
     this.isAttacking = false;
     this.attackBox = {
-      position: this.position,
+      position: {
+        x: this.position.x,
+        y: this.position.y
+      },
+      offset,
       width: 100,
       height: 50
     }
@@ -44,6 +48,9 @@ class Sprite {
   update(){
     this.draw()
     
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     //this.position.y + this.height é o pé do personagem
@@ -76,6 +83,10 @@ const player = new Sprite({
   velocity: {
     x: 0,
     y: 0
+  },
+  offset: {
+    x: 0,
+    y: 0
   }
 })
 
@@ -89,7 +100,11 @@ const enemy = new Sprite({
     x: 0,
     y: 0
   },
-  color: 'blue'
+  color: 'blue',
+  offset: {
+    x: -50,
+    y: 0
+  }
 })
 
 const keys = {
@@ -111,6 +126,18 @@ const keys = {
   ArrowUp: {
     pressed: false
   }
+}
+
+function rectangularCollision({
+  rectangle1,
+  rectangle2
+}){
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
+    rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
 }
 
 //animate the entire canvas
@@ -139,14 +166,24 @@ function animate(){
   }
 
   //detect for collision
-  if(player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
-    player.attackBox.position.x <= enemy.position.x + enemy.width &&
-  player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-  player.attackBox.position.y <= enemy.position.y + enemy.height &&
+  if(rectangularCollision({
+    rectangle1: player,
+    rectangle2: enemy
+  }) &&
   player.isAttacking)
   {
     player.isAttacking = false;
     console.log('hit');
+  }
+
+  if(rectangularCollision({
+    rectangle1: enemy,
+    rectangle2: player
+  }) &&
+  enemy.isAttacking)
+  {
+    enemy.isAttacking = false;
+    console.log('hitE');
   }
 }
 
@@ -186,6 +223,9 @@ window.addEventListener("keydown", (event)=>{
         enemy.velocity.y = -STRENGHT_J;
         enemy.isJumping = true;
       }
+    break;
+    case 'ArrowDown':
+      enemy.attack();
     break;
   }
 })
