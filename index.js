f81656c82d5f5e8cef5a18b8a53a91afec8b0883
Isabela Coelho <isabelacoelhoo@gmail.com>
2025-02-12 12:01:00 -0300

    create movement left and right

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.2;
 
-///creating a player
+///creating a sprite class for players and enemies
 class Sprite {
   constructor({position, velocity}) {
     this.position = position;
@@ -25,6 +25,7 @@ class Sprite {
   update(){
     this.draw()
     
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     //this.position.y + this.height é o pé do personagem
     //se o pé do personagem + a altura que ele ta do chão é maior ou igual que o fim do canvas
@@ -38,6 +39,7 @@ class Sprite {
   }
 }
 
+//create player
 const player = new Sprite({
   position: {
     x: 0,
@@ -49,6 +51,7 @@ const player = new Sprite({
   }
 })
 
+//create enemy
 const enemy = new Sprite({
   position: {
     x: 400,
@@ -60,12 +63,58 @@ const enemy = new Sprite({
   }
 })
 
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+let lastKey;
+
+//animate the entire canvas
 function animate(){
   window.requestAnimationFrame(animate);
+  
   c.fillStyle = 'black';
   c.fillRect(0,0, canvas.width, canvas.height);
+  
   player.update();
   enemy.update();
+
+  player.velocity.x = 0;
+
+  if(keys.a.pressed && lastKey === 'a'){
+    player.velocity.x = -1;
+  } else if(keys.d.pressed && lastKey === 'd'){
+    player.velocity.x = 1;
+  }
 }
 
 animate();
+
+window.addEventListener("keydown", (event)=>{
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true;
+      lastKey = 'd';
+    break;
+    case 'a':
+      keys.a.pressed = true;
+      lastKey = 'a';
+    break;
+  }
+})
+
+window.addEventListener("keyup", (event)=>{
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false;
+    break;
+    case 'a':
+      keys.a.pressed = false;
+    break;
+  }
+})
